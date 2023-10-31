a8e1966922662cdfb2ccf646b2a3a45a19ac9efe
Aayush9029 <developer@aayush.art>
2023-10-31 11:53:32 -0400

    Initial Commit

---
+++ YorkURec/YorkURecApp.swift
@@ -0,0 +1,17 @@
+//
+//  YorkURecApp.swift
+//  YorkURec
+//
+//  Created by Aayush Pokharel on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct YorkURecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
